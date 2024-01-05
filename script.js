d7a0c7b4c69278c8b1e96d4aa11329b1c352b50e
unknown <qpscm662@naver.com>
2024-01-05 16:07:58 +0900

    alert ID값 구현완료

--- script.js
+++ script.js
@@ -29,30 +29,19 @@ function moviesApi(response) {
     let voteAverage = response[i]['vote_average'];
     let id = response[i]['id'];
 
-    let temp_hteml = `<div class="col">
-      <a class="alink" data-linkBtn="${id}" href="#">
-         <div class="card h-100">
-           <img src="https://image.tmdb.org/t/p/w500${posterPath}" >
-           <div class="card-body">
-            <h5 class="card-title">${movieTitle}</h5>
-            <p class="card-text">${movieOverview}</p>
-          <p class="card-average">${voteAverage}</p>
-          </div>
-        </div>
-      </a>
+    let temp_hteml = `<div class="col" >
+    <a href = "#"><div class="card h-100" onclick="alert('ID 주소는: ${id} 입니다.')">
+    <img src="https://image.tmdb.org/t/p/w500${posterPath}"> 
+    <div class="card-body">
+    <h5 class="card-title" >${movieTitle}</h5>
+    <p class="card-text" >${movieOverview}</p>
+    <p class="card-average" >${voteAverage}</p>
+      </div>
     </div>
-  `;
-    cardList.insertAdjacentHTML('beforeend', temp_hteml);
-  }
-  let alink = document.querySelectorAll('.alink');
+    </a> 
+  </div>
+`;
 
-  for (let j = 0; j < id.length; j++) {
-    alink.addEventListener('click', function (e) {
-      e.preventDefault(); //a태그 막아. Html도 클릭이라서 이중클릭됨.
-      let idfor = alink[j];
-      console.log(idfor);
-      const result = id.getAttribute('data-linkBtn');
-      alert(`ID명은 ${result} 입니다.`);
-    });
+    cardList.insertAdjacentHTML('beforeend', temp_hteml);
   }
 }
